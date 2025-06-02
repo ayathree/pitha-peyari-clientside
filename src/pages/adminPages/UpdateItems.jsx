95f01c7caf4d1de3f5cea277187b7085cc83654c
ayathree <nobanitaayathree333@gmail.com>
2025-06-02 23:08:03 +0600

    updated update item page

--- src/pages/adminPages/UpdateItems.jsx
+++ src/pages/adminPages/UpdateItems.jsx
@@ -119,7 +119,7 @@ const UpdateItems = () => {
                             <label className="text-yellow-600 dark:text-gray-200">Saving Price</label>
                             <input
                                 name="customerSave"
-                                value={formData.customerSave.toFixed(2)}
+                                value={formData.customerSave}
                                 type="number"
                                 disabled
                                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
