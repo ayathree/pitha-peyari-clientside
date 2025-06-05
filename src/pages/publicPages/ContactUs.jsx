bbd365f849d2691b2c8edefe9f594cbce5ed8cb6
ayathree <nobanitaayathree333@gmail.com>
2025-06-05 12:48:56 +0600

    customize the contact page

--- src/pages/publicPages/ContactUs.jsx
+++ src/pages/publicPages/ContactUs.jsx
@@ -2,8 +2,46 @@
 
 const ContactUs = () => {
     return (
-        <div>
-            <p>Contact Us</p>
+         <div>
+             <div className="mt-10 relative">
+          <img className="object-cover w-full min-h-screen h-[10vh]" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGupkj_ZAjI4pmTA3ALIOI5GairFNT16AbTw&s" alt="" />
+          
+          
+          <div className="flex justify-center items-center">
+            <p className="font-bold text-5xl capitalize mt-2 absolute  top-52 p-2 bg-white text-yellow-600">Contact us</p>
+          </div>
+          </div>
+          <div className="mt-20">
+    <h2 className="text-xl underline font-semibold text-yellow-600 capitalize dark:text-white text-center">Contact Us</h2>
+            <section className="max-w-4xl p-6 mx-auto bg-white rounded-md  dark:bg-gray-800">
+
+    <form onSubmit={''}>
+        <div className="grid grid-cols-1 gap-6 mt-4 ">
+            <div>
+                <label className="text-yellow-600 dark:text-gray-200" >Name</label>
+                <input id="username" name="name" required type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+            </div>
+
+            <div>
+                <label className="text-yellow-600 dark:text-gray-200" >Email Address</label>
+                <input id="emailAddress" name="email" required type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+            </div>
+
+            <div>
+                <label className="text-yellow-600 dark:text-gray-200">Message</label>
+                <textarea id="text" name="message" required type="text" className="block w-full h-[40vh] px-4 py-5 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+            </div>
+
+            
+        </div>
+
+        <div className=" mt-16">
+            <button className="px-8 py-2.5 h-[10vh] leading-5 text-white font-bold transition-colors duration-300 transform bg-yellow-600 outline outline-1 hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Send</button>
+        </div>
+    </form>
+</section>
+          </div>
+            
         </div>
     );
 };
